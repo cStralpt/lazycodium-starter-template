19ddd86042de61d111c0f621fbacce5bf53aea54
Satria Alipatullah <satrialipatullah@gmail.com>
2023-08-27 16:32:32 +0700

    remove unused config

--- after/ftplugin/html.lua
+++
@@ -1,6 +0,0 @@
-local spec_treesitter = require("mini.ai").gen_spec.treesitter
-vim.b.miniai_config = {
-  custom_textobjects = {
-    t = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
-  },
-}
