5b2e703d813baefd9d43cecff430e03a09d2519f
Satria Alipatullah <satrialipatullah@gmail.com>
2023-10-17 14:38:35 +0700

    add: new theme(rose-pine)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,20 +1,21 @@
 return {
-  { "nxvu699134/vn-night.nvim" },
-  { "folke/tokyonight.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
+	{ "nxvu699134/vn-night.nvim" },
+	{ "folke/tokyonight.nvim" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "rose-pine",
+		},
+	},
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+	{ "rose-pine/neovim", name = "rose-pine" },
 }
