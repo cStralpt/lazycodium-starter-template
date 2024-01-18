6224b4a06361db204d995b0f98d5e60c493c5725
Satria Alipatullah <satrialipatullah@gmail.com>
2024-01-18 22:38:09 +0700

    custom hi group for flash nvim

--- lua/plugins/flashMotion.lua
+++ lua/plugins/flashMotion.lua
@@ -1,21 +1,32 @@
+local function setupCustomHighlightGroup()
+	vim.api.nvim_command("hi clear FlashMatch")
+	vim.api.nvim_command("hi clear FlashCurrent")
+	vim.api.nvim_command("hi clear FlashLabel")
+
+	vim.api.nvim_command("hi FlashMatch guibg=#4A47A3 guifg=#B8B5FF") -- Emerald background
+	vim.api.nvim_command("hi FlashCurrent guibg=#456268 guifg=#D0E8F2")
+	vim.api.nvim_command("hi FlashLabel guibg=#A25772 guifg=#EEF5FF")
+end
+
 return {
-  "folke/flash.nvim",
-  ---@type Flash.Config
-  opts = {
-    rainbow = {
-      enabled = true,
-      -- number between 1 and 9
-      shade = 5,
-    },
-    highlight = {
-      --- backdrop option enabled will lag in vscode
-      backdrop = true,
-      groups = {
-        match = "FlashMatch",
-        current = "FlashCurrent",
-        backdrop = "FlashBackdrop",
-        label = "FlashCurrent",
-      },
-    },
-  },
+	"folke/flash.nvim",
+	---@type Flash.Config
+	opts = {
+		rainbow = {
+			enabled = true,
+			shade = 5,
+		},
+		highlight = {
+			backdrop = true,
+			groups = {
+				match = "FlashMatch",
+				current = "FlashCurrent",
+				backdrop = "FlashBackdrop",
+				label = "FlashLabel",
+			},
+		},
+	},
+	config = function()
+		setupCustomHighlightGroup()
+	end,
 }
