8b6a13d39e32b75dcf8cae5d3233ffec2712cdcd
Satria Alipatullah <satrialipatullah@gmail.com>
2024-02-10 03:48:39 +0700

    refactoring

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,11 +15,25 @@ local function map(mode, lhs, rhs, opts)
 	end
 end
 
+local function ToggleWordWrap()
+	if vim.wo.wrap then
+		vim.wo.wrap = false
+		print("Word wrap disabled")
+	else
+		vim.wo.wrap = true
+		print("Word wrap enabled")
+	end
+end
+
 local function copyToClipBoard()
 	vim.cmd("set clipboard+=unnamedplus")
 	vim.cmd("norm! y")
 	vim.cmd("set clipboard-=unnamedplus")
 	print("copied!")
+end
+
+local function callVSCodeFunction(vsCodeCommand)
+	vim.cmd(vsCodeCommand)
 end
 
 map("i", "<C-a>", function()
@@ -31,115 +45,7 @@ map({ "v", "i" }, "<C-c>", function()
 end, { remap = false, desc = "copy selected text" })
 map("i", "<BS>", "<cmd>norm! de<CR>", { noremap = true, desc = "delete next word to right" })
 map("i", "<C-l>", "<Del>", { remap = true, desc = "delete one character backward" })
-
-if vim.g.vscode then
-	print("⚡connected to neovim!💯‼️🤗😎")
-	-- this shortcut somehow will jump/focus into terminal
-	-- map(
-	--   "i",
-	--   "<C-j>",
-	--   "<Cmd>call VSCodeCall('workbench.action.terminal.focus')<CR>",
-	--   { noremap = true, silent = true, desc = "vs code keybinding test" }
-	-- )
-
-	map(
-		"n",
-		"<C-/>",
-		"<Cmd>call VSCodeCall('workbench.action.terminal.focus')<CR>",
-		{ noremap = true, silent = true, desc = "vs code keybinding test" }
-	)
-
-	map("n", "<S-l>", function()
-		local cyclesNextEditor = "call VSCodeNotify('workbench.action.nextEditor')"
-		vim.cmd(cyclesNextEditor)
-	end, { noremap = true, desc = "switch between editor to next" })
-
-	map("n", "<S-h>", function()
-		local cyclesToNextEditor = "call VSCodeNotify('workbench.action.previousEditor')"
-		vim.cmd(cyclesToNextEditor)
-	end, { noremap = true, desc = "switch between editor to previous" })
-
-	map("n", "gr", function()
-		local go2Referces = "call VSCodeNotify('editor.action.referenceSearch.trigger')"
-		vim.cmd(go2Referces)
-	end, { noremap = true, desc = "peek references inside vs code" })
-
-	map("n", "<leader>sd", function()
-		local openProblemsInfos = "call VSCodeNotify('workbench.action.problems.focus')"
-		vim.cmd(openProblemsInfos)
-	end, { noremap = true, desc = "open problems and errors infos" })
-
-	map("n", "<leader>e", function()
-		local openFileExplorer = "call VSCodeNotify('workbench.files.action.focusFilesExplorer')"
-		vim.cmd(openFileExplorer)
-	end, { noremap = true, desc = "focus to file explorer" })
-
-	map("n", "<leader>fe", function()
-		local openFileExplorer = "call VSCodeNotify('workbench.files.action.focusFilesExplorer')"
-		vim.cmd(openFileExplorer)
-	end, { noremap = true, desc = "focus to file explorer" })
-
-	map("n", "<leader>ff", function()
-		local quickOpenFiles = "call VSCodeNotify('workbench.action.quickOpen')"
-		vim.cmd(quickOpenFiles)
-	end, { noremap = true, desc = "open files" })
-
-	map("n", "<leader>gg", function()
-		local openGitSourceControl = "call VSCodeNotify('workbench.view.scm')"
-		vim.cmd(openGitSourceControl)
-	end, { noremap = true, desc = "open git source controll" })
-
-	map("n", "<leader>sml", function()
-		local bookmarksList4CurrentFile = "call VSCodeNotify('bookmarks.list')"
-		vim.cmd(bookmarksList4CurrentFile)
-	end, { noremap = true, desc = "open bookmarks list for current files" })
-
-	map("n", "<leader>smL", function()
-		local bookmarksList4AllFiles = "call VSCodeNotify('bookmarks.listFromAllFiles')"
-		vim.cmd(bookmarksList4AllFiles)
-	end, { noremap = true, desc = "open bookmarks list for all files" })
-
-	map("n", "<leader>smm", function()
-		local bookmarksMarkToggle = "call VSCodeNotify('bookmarks.toggle')"
-		vim.cmd(bookmarksMarkToggle)
-	end, { noremap = true, desc = "toggle bookmarks" })
-
-	map("n", "<leader>smd", function()
-		local action = "call VSCodeNotify('bookmarks.clear')"
-		vim.cmd(action)
-	end, { noremap = true, desc = "clear bookmarks from current file" })
-
-	map("n", "<leader>smr", function()
-		local action = "call VSCodeNotify('bookmarks.clearFromAllFiles')"
-		vim.cmd(action)
-	end, { noremap = true, desc = "clear bookmarks from all file" })
-
-	map("n", "<leader>cr", function()
-		local action = "call VSCodeNotify('editor.action.rename')"
-		vim.cmd(action)
-	end, { noremap = true, desc = "rename symbol" })
-
-	map("n", "<leader>ca", function()
-		local codeAction4QuickFix = "call VSCodeNotify('editor.action.quickFix')"
-		vim.cmd(codeAction4QuickFix)
-	end, { noremap = true, desc = "open quick fix in vs code" })
-
-	map("n", "<leader>cA", function()
-		local openSourceAction = "call VSCodeNotify('editor.action.sourceAction')"
-		vim.cmd(openSourceAction)
-	end, { noremap = true, desc = "open source Action in vs code" })
-
-	map("n", "<leader>cd", function()
-		local openSourceAction = "call VSCodeNotify('workbench.panel.markers.view.focus')"
-		vim.cmd(openSourceAction)
-	end, { noremap = true, desc = "open problems diagnostics" })
-
-	map({ "v" }, "<C-c>", function()
-		local copyText = "call VSCodeNotify('editor.action.clipboardCopyAction')"
-		vim.cmd(copyText)
-		print("📎added to clipboard!")
-	end, { noremap = true, desc = "copy text/add text to clipboard" })
-else
+local function neovimMappings()
 	map(
 		{ "i", "t" },
 		"<C-j>",
@@ -155,18 +61,10 @@ else
 
 	map("i", "<C-f>", "<Esc>/", { noremap = false })
 
-	function ToggleWordWrap()
-		if vim.wo.wrap then
-			vim.wo.wrap = false
-			print("Word wrap disabled")
-		else
-			vim.wo.wrap = true
-			print("Word wrap enabled")
-		end
-	end
-
 	-- Map a keybinding to toggle word wrap
-	map("n", "<leader>ct", ":lua ToggleWordWrap()<CR>", { noremap = true, silent = true, desc = "toggle word wrap" })
+	map("n", "<leader>ct", function()
+		ToggleWordWrap()
+	end, { noremap = true, silent = true, desc = "toggle word wrap" })
 	map("n", "<leader>bc", "<cmd>BufferLinePick<CR>", { noremap = false, silent = true, desc = "pick buffer" })
 	map("n", "-", require("oil").open, { desc = "Open parent directory" })
 	-- force/replace already used keymaps
@@ -176,4 +74,90 @@ else
 		"<cmd>AerialNavOpen<CR>",
 		{ noremap = true, silent = true, desc = "Symbols Outline(Aerial)" }
 	)
+end
+
+local function vscodeMappings()
+	map("n", "<C-/>", function()
+		callVSCodeFunction("call VSCodeCall('workbench.action.terminal.focus')")
+	end, { noremap = true, silent = true, desc = "vs code keybinding test" })
+
+	map("n", "<S-l>", function()
+		callVSCodeFunction("call VSCodeNotify('workbench.action.nextEditor')")
+	end, { noremap = true, desc = "switch between editor to next" })
+
+	map("n", "<S-h>", function()
+		callVSCodeFunction("call VSCodeNotify('workbench.action.previousEditor')")
+	end, { noremap = true, desc = "switch between editor to previous" })
+
+	map("n", "gr", function()
+		callVSCodeFunction("call VSCodeNotify('editor.action.referenceSearch.trigger')")
+	end, { noremap = true, desc = "peek references inside vs code" })
+
+	map("n", "<leader>sd", function()
+		callVSCodeFunction("call VSCodeNotify('workbench.action.problems.focus')")
+	end, { noremap = true, desc = "open problems and errors infos" })
+
+	map("n", "<leader>e", function()
+		callVSCodeFunction("call VSCodeNotify('workbench.files.action.focusFilesExplorer')")
+	end, { noremap = true, desc = "focus to file explorer" })
+
+	map("n", "<leader>fe", function()
+		callVSCodeFunction("call VSCodeNotify('workbench.files.action.focusFilesExplorer')")
+	end, { noremap = true, desc = "focus to file explorer" })
+
+	map("n", "<leader>ff", function()
+		callVSCodeFunction("call VSCodeNotify('workbench.action.quickOpen')")
+	end, { noremap = true, desc = "open files" })
+
+	map("n", "<leader>gg", function()
+		callVSCodeFunction("call VSCodeNotify('workbench.view.scm')")
+	end, { noremap = true, desc = "open git source control" })
+
+	map("n", "<leader>sml", function()
+		callVSCodeFunction("call VSCodeNotify('bookmarks.list')")
+	end, { noremap = true, desc = "open bookmarks list for current files" })
+
+	map("n", "<leader>smL", function()
+		callVSCodeFunction("call VSCodeNotify('bookmarks.listFromAllFiles')")
+	end, { noremap = true, desc = "open bookmarks list for all files" })
+
+	map("n", "<leader>smm", function()
+		callVSCodeFunction("call VSCodeNotify('bookmarks.toggle')")
+	end, { noremap = true, desc = "toggle bookmarks" })
+
+	map("n", "<leader>smd", function()
+		callVSCodeFunction("call VSCodeNotify('bookmarks.clear')")
+	end, { noremap = true, desc = "clear bookmarks from current file" })
+
+	map("n", "<leader>smr", function()
+		callVSCodeFunction("call VSCodeNotify('bookmarks.clearFromAllFiles')")
+	end, { noremap = true, desc = "clear bookmarks from all file" })
+
+	map("n", "<leader>cr", function()
+		callVSCodeFunction("call VSCodeNotify('editor.action.rename')")
+	end, { noremap = true, desc = "rename symbol" })
+
+	map("n", "<leader>ca", function()
+		callVSCodeFunction("call VSCodeNotify('editor.action.quickFix')")
+	end, { noremap = true, desc = "open quick fix in vs code" })
+
+	map("n", "<leader>cA", function()
+		callVSCodeFunction("call VSCodeNotify('editor.action.sourceAction')")
+	end, { noremap = true, desc = "open source Action in vs code" })
+
+	map("n", "<leader>cd", function()
+		callVSCodeFunction("call VSCodeNotify('workbench.panel.markers.view.focus')")
+	end, { noremap = true, desc = "open problems diagnostics" })
+
+	map({ "v" }, "<C-c>", function()
+		callVSCodeFunction("call VSCodeNotify('editor.action.clipboardCopyAction')")
+		print("📎added to clipboard!")
+	end, { noremap = true, desc = "copy text/add text to clipboard" })
+end
+
+if vim.g.vscode then
+	print("⚡connected to neovim!💯‼️🤗😎")
+	vscodeMappings()
+else
+	neovimMappings()
 end
